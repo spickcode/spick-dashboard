610428845db7878aae2c12e3ba9f362b66c065ce
spickcode <spickcode@gmail.com>
2022-09-22 01:47:27 +0200

    ContextAPI

---
+++ src/contexts/ContextProvider.js
@@ -0,0 +1,22 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const StateContext = createContext();
+
+const initialState = {
+  chat: false,
+  cart: false,
+  userProfile: false,
+  notification: false,
+};
+
+export const ContextProvider = ({ children }) => {
+  const [activeMenu, seTActiveMenu] = useState(true);
+
+  return (
+    <StateContext.Provider value={{ activeMenu, seTActiveMenu }}>
+      {children}
+    </StateContext.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(StateContext);
